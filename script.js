63deedeba217cadc9a72850e238ed095b67837d9
Jorge Kimura <kimurajorge2001@gmail.com>
2022-05-12 11:26:59 -0300

    Arquivo JS - Função e verificações

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-function frutas (array){
-  // const mainFrutas = document.querySelector('.fruits')
-    const ulFrutas = document.querySelector('.secao-frutas')
+// Função que recebe um array de objetos (array de objetos de produtos que está no products.js)
+
+function categoriaProdutos (array){
+    const ulFrutas = document.querySelector('.secao-frutas') // criou-se no HTML a classe e só selecionou para armazenar em uma variavel
     
-    const ulBebidas = document.querySelector('.secao-bebidas')
+    const ulBebidas = document.querySelector('.secao-bebidas') // criou-se no HTML a classe e só selecionou para armazenar em uma variavel
 
-    const ulHigiene = document.querySelector('.secao-higiene')
-
-    array.forEach(item => {
-       if(item.category == 'Frutas'){
-       ulFrutas.innerHTML += `<li class="product">
+    const ulHigiene = document.querySelector('.secao-higiene') // criou-se no HTML a classe e só selecionou para armazenar em uma variavel
+  //array de objetos.método(função (parâmetro) {o que deve fazer}) 
+    array.forEach(item => { // Para percorrer o array de objetos e fazer a vericaçãp juntamente com a lista e outros elementos HTML
+       if(item.category == 'Frutas'){ // verificação caso a categoria seja frutas deve-se criar uma lista e incrementar na ul de frutas 
+       ulFrutas.innerHTML += `<li class="product">  
        <img
          src=${item.image}
          alt=""
@@ -22,7 +23,7 @@ function frutas (array){
        </main>
      </li>`
     }
-    if(item.category == 'Bebidas'){
+    if(item.category == 'Bebidas'){ // verificação caso a categoria seja bebidas deve-se criar uma lista e incrementar na ul de bebidas
         ulBebidas.innerHTML += `<li class="product">
         <img
           src=${item.image}
@@ -37,7 +38,7 @@ function frutas (array){
         </main>
       </li>`
     }
-    if(item.category == 'Higiene'){
+    if(item.category == 'Higiene'){ // verificação caso a categoria seja higiene deve-se criar uma lista e incrementar na ul de higiene
         ulHigiene.innerHTML += `<li class="product">
         <img
           src=${item.image}
@@ -55,4 +56,4 @@ function frutas (array){
 })  
     
 }
-frutas(products)
+categoriaProdutos(products)
